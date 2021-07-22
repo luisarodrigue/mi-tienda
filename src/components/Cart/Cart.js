c0698462e06372a8aade520a7505ba490e1740d1
luisarodrigue <microfonow@gmail.com>
2021-07-21 23:49:28 -0500

    las ordenees se generan con éxito, pero aún flta organizar y actualizar los stocks

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -7,8 +7,14 @@ import { CartContext } from '../../context/CartContext';
 import CartItem from './ItemCart/CartItem';
 import ListGroup from 'react-bootstrap/esm/ListGroup';
 import Button from 'react-bootstrap/esm/Button';
+import { getFirestore } from '../../firebase/firebase';
+import firebase from 'firebase/app';
+import '@firebase/firestore';
+
+
 
 function Cart() {
+    const buyer={name: "usuario de prueba", phone: 3137897895, email: "estecorreono@esreal.xd"};
 
     const cart = useContext(CartContext);
     const [lista, setLista] = useState([]);
@@ -22,6 +28,39 @@ function Cart() {
         console.log(aux);
         setLista(aux);
     }, [cart]);
+
+    function submit(){
+        const db = getFirestore();
+        const orders = db.collection('orders');
+
+        const sub = cart.subTotal();
+        const newOrder = {
+            buyer,
+            items: cart.carrito.map((item)=>{
+                return item.item;
+            }),
+            date: firebase.firestore.Timestamp.fromDate(new Date()),
+            total: sub
+        };
+
+        console.log(newOrder);
+
+        orders.add(newOrder);
+
+        const items = db.collection('productos')
+        .where(firebase.firestore.FieldPath.documentId(), 'in' , cart.carrito.map(i => i.item.id));
+
+        items.get().then((query)=>{
+            const batch = db.batch();
+
+            query.docs.forEach((doc)=>{
+                if(doc.data().stock >= cart.carrito.quantity){
+                    batch.update(doc.ref, {stock: doc.data().stock - cart.carrito.quantity});
+                }
+            });
+        });
+
+    }
 
     return (
         <div>
@@ -44,7 +83,7 @@ function Cart() {
                     </Row>
                     <Row>
                         SubTotal: ${cart.subTotal()}
-                        <Button>Terminar compra</Button>
+                        <Button onClick={submit}>Terminar compra</Button>
                     </Row>
                 </>)
                 }
